3ec43b85b20cbb77df00fcf32328788be485cec8
Wai ManChan <waiman903@gmail.com>
2018-06-06 10:49:07 +0800

    add fast lane

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,7 @@
+var appIdentifier: String { return "KcinApp.TestJenkins" } // The bundle identifier of your app
+var appleID: String { return "donatsu.info@gmail.com" } // Your Apple email address
+
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
