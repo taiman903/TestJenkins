c269c79e6f52ecd7d7752bd0bb70d1588cb023c2
Wai ManChan <waiman903@gmail.com>
2018-06-06 09:48:09 +0800

    init

---
+++ TestJenkins/TestJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Wai ManChan on 6/6/2018.
+//  Copyright © 2018 KcinApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
